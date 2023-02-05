5869de569510119aec1eb2e925eb603bd8f6cee6
codingwithreza <codingwithreza@gmail.com>
2023-02-05 13:10:44 +0330

    make fields exportable!

--- external/redis/client.go
+++ external/redis/client.go
@@ -9,12 +9,12 @@ import (
 )
 
 type ClientImpl struct {
-	rdb      *redis.Client
-	duration time.Duration
+	Rdb      *redis.Client
+	Duration time.Duration
 }
 
 func (c *ClientImpl) CountUsersInSegment(context context.Context, title string) (*pkg.UserCountBySegmentResponse, error) {
-	count, rErr := c.rdb.Get(context, title).Result()
+	count, rErr := c.Rdb.Get(context, title).Result()
 	if rErr != nil {
 		return nil, rErr
 	}
@@ -26,18 +26,18 @@ func (c *ClientImpl) CountUsersInSegment(context context.Context, title string) 
 }
 
 func (c *ClientImpl) StoreUserCountInSegment(context context.Context, data pkg.UserCountBySegmentResponse) {
-	c.rdb.Set(context, data.Segment, data.Count, c.duration)
+	c.Rdb.Set(context, data.Segment, data.Count, c.Duration)
 }
 
 func (c *ClientImpl) ClearUserCacheInSegment(context context.Context, title string) {
-	c.rdb.Del(context, title)
+	c.Rdb.Del(context, title)
 }
 
 func (c *ClientImpl) StoreUserCountsInAllSegment(ctx context.Context, data []pkg.UserCountBySegmentResponse) error {
-	pipe := c.rdb.Pipeline()
+	pipe := c.Rdb.Pipeline()
 	for _, item := range data {
 		// Queue commands in the pipeline
-		pipe.Set(ctx, item.Segment, item.Count, c.duration)
+		pipe.Set(ctx, item.Segment, item.Count, c.Duration)
 	}
 	// Execute all commands in the pipeline
 	_, err := pipe.Exec(context.Background())
