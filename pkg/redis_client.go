377d3d6a070b66ccfa3565aa4660ab972c50d5c0
codingwithreza <codingwithreza@gmail.com>
2023-02-05 20:01:17 +0330

    delete files!

--- pkg/redis_client.go
+++
@@ -1,9 +0,0 @@
-package pkg
-
-import "context"
-
-type Redis interface {
-	CountUsersInSegment(context context.Context, title string) (*UserCountBySegmentResponse, error)
-	StoreUserCountInSegment(context context.Context, data UserCountBySegmentResponse)
-	ClearUserCacheInSegment(ctx context.Context, title string)
-}
