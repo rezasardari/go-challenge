6edc57ef2f6d6e8112c55128447916bd1d6d42e2
codingwithreza <codingwithreza@gmail.com>
2023-02-06 15:16:28 +0330

    some tests added for CountUsersInSegment service!

--- go.mod
+++ go.mod
@@ -56,6 +56,7 @@ require (
 	github.com/spf13/cast v1.5.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
 	github.com/subosito/gotenv v1.4.2 // indirect
 	github.com/ugorji/go/codec v1.2.9 // indirect
 	go.uber.org/atomic v1.10.0 // indirect
---
+++ pkg/service_test.go
@@ -0,0 +1,106 @@
+package pkg
+
+import (
+	"context"
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"testing"
+)
+
+type RepositoryMock struct {
+	mock.Mock
+}
+
+func (r *RepositoryMock) CountUsersInSegment(ctx context.Context, req UserCountBySegmentRequest) (*UserCountBySegmentResponse, error) {
+	args := r.Called(ctx, req)
+	return args.Get(0).(*UserCountBySegmentResponse), args.Error(1)
+}
+
+func (r *RepositoryMock) StoreUserInSegment(ctx context.Context, req StoreUserSegmentRequest) error {
+	args := r.Called(ctx, req)
+	return args.Error(0)
+}
+
+func (r *RepositoryMock) ArchiveExpiredData(ctx context.Context) error {
+	args := r.Called(ctx)
+	return args.Error(0)
+}
+
+func (r *RepositoryMock) CountUsersInAllSegments(ctx context.Context) ([]UserCountBySegmentResponse, error) {
+	args := r.Called(ctx)
+	return args.Get(0).([]UserCountBySegmentResponse), args.Error(1)
+}
+
+type RedisMock struct {
+	mock.Mock
+}
+
+func (c *RedisMock) CountUsersInSegment(context context.Context, title string) (*UserCountBySegmentResponse, error) {
+	args := c.Called(context, title)
+	return args.Get(0).(*UserCountBySegmentResponse), args.Error(1)
+}
+func (c *RedisMock) StoreUserCountInSegment(context context.Context, data UserCountBySegmentResponse) {
+	c.Called(context, data)
+	return
+}
+func (c *RedisMock) ClearUserCacheInSegment(ctx context.Context, title string) {
+	c.Called(ctx, title)
+	return
+}
+func (c *RedisMock) StoreUserCountsInAllSegment(ctx context.Context, data []UserCountBySegmentResponse) error {
+	args := c.Called(ctx, data)
+	return args.Error(0)
+}
+
+func Test_GetUserCountBySegmentation_WhenCacheNotNull(t *testing.T) {
+	asserts := assert.New(t)
+
+	redisMock := new(RedisMock)
+	repositoryMock := new(RepositoryMock)
+
+	sut := ServiceImpl{
+		Repository: repositoryMock,
+		Redis:      redisMock,
+	}
+
+	redisMock.On("CountUsersInSegment", mock.Anything, mock.Anything).Return(&UserCountBySegmentResponse{
+		Segment: "sport",
+		Count:   10,
+	}, nil)
+
+	res, err := sut.GetUserCountBySegmentation(context.Background(), UserCountBySegmentRequest{
+		Name: "sport",
+	})
+
+	asserts.Nil(err)
+	asserts.NotNil(res)
+	redisMock.AssertCalled(t, "CountUsersInSegment", mock.Anything, mock.Anything)
+	repositoryMock.AssertNotCalled(t, "CountUsersInSegment", mock.Anything, mock.Anything)
+}
+
+func Test_GetUserCountBySegmentation_WhenCacheIsNull(t *testing.T) {
+	asserts := assert.New(t)
+
+	redisMock := new(RedisMock)
+	repositoryMock := new(RepositoryMock)
+
+	sut := ServiceImpl{
+		Repository: repositoryMock,
+		Redis:      redisMock,
+	}
+
+	redisResponse := &UserCountBySegmentResponse{}
+	redisMock.On("CountUsersInSegment", mock.Anything, mock.Anything).Return(redisResponse, errors.New("redisErr"))
+	repositoryMock.On("CountUsersInSegment", mock.Anything, mock.Anything).Return(&UserCountBySegmentResponse{
+		Segment: "sport",
+		Count:   10,
+	}, nil)
+	res, err := sut.GetUserCountBySegmentation(context.Background(), UserCountBySegmentRequest{
+		Name: "sport",
+	})
+	asserts.NotNil(res)
+	asserts.Nil(err)
+	redisMock.AssertCalled(t, "CountUsersInSegment", mock.Anything, mock.Anything)
+	repositoryMock.AssertCalled(t, "CountUsersInSegment", mock.Anything, mock.Anything)
+}
